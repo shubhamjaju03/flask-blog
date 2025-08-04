94833df2b2fa94f98e313ac1323282227b594934
Shubh <shubh@example.com>
2025-08-04 16:17:37 +0530

    Initial commit from Day 71 blog

--- main.py
+++ main.py
@@ -39,8 +39,7 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return db.get_or_404(User, user_id)
-
+    return User.query.get(int(user_id))
 
 # For adding profile images to the comment section
 gravatar = Gravatar(app,
