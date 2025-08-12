14a68f0df3abceca1d9dc80a01fe9ce485163384
Shubham Jaju <143123082+shubhamjaju03@users.noreply.github.com>
2025-08-12 13:41:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ with app.app_context():
 def admin_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if not current_user.is_authenticated or current_user.id != 2:
+        if not current_user.is_authenticated or current_user.id != 1:
             return abort(403)
         return f(*args, **kwargs)
     return decorated_function
